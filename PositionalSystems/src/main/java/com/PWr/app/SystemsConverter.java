635ca362523dd8f1c65279cbb09a1b5aaca32971
Jakub Musiał <jmusial2003@gmail.com>
2022-10-19 09:59:18 +0200

    SystemsConverter.addSystem: Added a condition checking whether the main system base has been set

--- PositionalSystems/src/main/java/com/PWr/app/SystemsConverter.java
+++ PositionalSystems/src/main/java/com/PWr/app/SystemsConverter.java
@@ -13,21 +13,26 @@ public class SystemsConverter {
 
 
     public SystemsConverter () {
+        this.p0 = 0;
+
         this.index = 0;
         this.P = new int[10];
     }
 
     public void setMainSystem(int base) {
         if (base > 1 && base < 17)  { this.p0 = base; }
-        else    { System.out.println("Could not add a new system base (" + base + ")!\nSystem base must be in range [2, 16]!"); }
+        else    { System.out.println("Could not set the main system base (" + base + ")!\nSystem base must be in range [2, 16]!"); }
     }
 
     public void addSystem(int base) {
-        if (this.index < 10) {
-            if (base > 1 && base < 17)  { this.P[this.index] = base; }
-            else    { System.out.println("Could not add a new system base (" + base + ")!\nSystem base must be in range [2, 16]!"); }
+        if (this.p0 == 0) {
+            if (this.index < 10) {
+                if (base > 1 && base < 17)  { this.P[this.index] = base; }
+                else    { System.out.println("Could not add a new system base (" + base + ")!\nSystem base must be in range [2, 16]!"); }
+            }
+            else    { System.out.println("Could not add a new system base! The limit has been reached!"); }
         }
-        else    { System.out.println("Could not add a new system base! The limit has been reached!"); }
+        else    { System.out.println("Could not add a new system base! First set the main system base"); }
     }
 }
 
