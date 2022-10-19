7ecf8411a9cafbcadfefa4d17f0e1d265677226a
Jakub Musiał <jmusial2003@gmail.com>
2022-10-19 10:29:31 +0200

    SystemsConverter.class: Added convert, convertToDecimal methods

--- PositionalSystems/src/main/java/com/PWr/app/SystemsConverter.java
+++ PositionalSystems/src/main/java/com/PWr/app/SystemsConverter.java
@@ -34,5 +34,29 @@ public class SystemsConverter {
         }
         else    { System.out.println("Could not add a new system base! First set the main system base"); }
     }
+
+
+        private String convertToDecimal (String number, int base) {
+            int dec_number = 0;
+
+            for (int i = number.length() - 1; i >= 0; i--) {
+                try {
+                    dec_number += Integer.valueOf(number.charAt(i)) * Math.pow(base, i);
+                } 
+                catch (NumberFormatException exception) { return exception.getMessage(); }
+            }
+
+            return Integer.toString(dec_number);
+        }
+
+    public String convert (String number, int base) {
+        if (this.p0 == 0) { return "Could not add a new system base! First set the main system base"; }
+
+        if (this.p0 != 10)  { number = convertToDecimal(number, base); }
+
+        if (base != 10) { number = convertToNonDecimal(number, base); }
+
+        return number;
+    }
 }
 
