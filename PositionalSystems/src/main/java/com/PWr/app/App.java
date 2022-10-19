b1d603351dd740a5ef4bf02e71f6379d7ad82ee7
Jakub Musiał <jmusial2003@gmail.com>
2022-10-19 11:45:11 +0200

    Creating an artificial change

--- PositionalSystems/src/main/java/com/PWr/app/App.java
+++ PositionalSystems/src/main/java/com/PWr/app/App.java
@@ -7,5 +7,7 @@ package com.PWr.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to 'Positional Systems Converter'");
+
+        // Adding a comment to create a artificial difference for an artificial merge conflict
     }
 }
