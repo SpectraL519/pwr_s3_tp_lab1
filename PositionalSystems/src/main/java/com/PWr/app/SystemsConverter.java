eac72e8e954c94457866d240db3b1e5d0a15160e
Jakub Musiał <jmusial2003@gmail.com>
2022-10-19 10:31:05 +0200

    SystemsConverter.java: Deleted index, P attributes and addSystem method

--- PositionalSystems/src/main/java/com/PWr/app/SystemsConverter.java
+++ PositionalSystems/src/main/java/com/PWr/app/SystemsConverter.java
@@ -7,32 +7,15 @@ package com.PWr.app;
 public class SystemsConverter {
     private int p0;
 
-    private int index;
-    private int[] P;
-
 
 
     public SystemsConverter () {
         this.p0 = 0;
-
-        this.index = 0;
-        this.P = new int[10];
     }
 
     public void setMainSystem(int base) {
         if (base > 1 && base < 17)  { this.p0 = base; }
         else    { System.out.println("Could not set the main system base (" + base + ")!\nSystem base must be in range [2, 16]!"); }
-    }
-
-    public void addSystem(int base) {
-        if (this.p0 == 0) {
-            if (this.index < 10) {
-                if (base > 1 && base < 17)  { this.P[this.index] = base; }
-                else    { System.out.println("Could not add a new system base (" + base + ")!\nSystem base must be in range [2, 16]!"); }
-            }
-            else    { System.out.println("Could not add a new system base! The limit has been reached!"); }
-        }
-        else    { System.out.println("Could not add a new system base! First set the main system base"); }
     }
 
 
