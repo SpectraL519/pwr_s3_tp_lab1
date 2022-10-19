5247b975a6763be54477262b1514bfa95fa10d2e
Jakub Musiał <jmusial2003@gmail.com>
2022-10-19 08:03:12 +0200

    SystemsConverter.java - Changed names of variables: main_sys_base -> p0, new_sys_bases -> P

--- PositionalSystems/src/main/java/com/PWr/app/SystemsConverter.java
+++ PositionalSystems/src/main/java/com/PWr/app/SystemsConverter.java
@@ -5,26 +5,26 @@ package com.PWr.app;
 
 
 public class SystemsConverter {
-    private int main_sys_base;
+    private int p0;
 
     private int index;
-    private int[] new_sys_bases;
+    private int[] P;
 
 
 
     public SystemsConverter () {
         this.index = 0;
-        this.new_sys_bases = new int[10];
+        this.P = new int[10];
     }
 
     public void setMainSystem(int base) {
-        if (base > 1 && base < 17)  { this.main_sys_base = base; }
+        if (base > 1 && base < 17)  { this.p0 = base; }
         else    { System.out.println("Could not add a new system base (" + base + ")!\nSystem base must be in range [2, 16]!"); }
     }
 
     public void addSystem(int base) {
         if (this.index < 10) {
-            if (base > 1 && base < 17)  { this.new_sys_bases[this.index] = base; }
+            if (base > 1 && base < 17)  { this.P[this.index] = base; }
             else    { System.out.println("Could not add a new system base (" + base + ")!\nSystem base must be in range [2, 16]!"); }
         }
         else    { System.out.println("Could not add a new system base! The limit has been reached!"); }
